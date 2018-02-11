4f42c5c05326e8a065b65de8fcb3503b40cae1a3
Patrick Elsen <pelsen@xfbs.net>
2018-02-11 14:58:20 +0100

    improved renderer design

---
+++ src/litmus/markd_renderer.cr
@@ -0,0 +1,48 @@
+require "markd"
+
+module Litmus
+  abstract class MarkdRenderer
+    alias Node = Markd::Node
+    alias Type = Markd::Node::Type
+    alias Handler = Proc(Node, IO, Nil)
+
+    @@handlers = {} of Type => Handler
+
+    def initialize
+    end
+
+    def render(node : Node, output : IO = IO::Memory.new)
+      loop do
+        handler = @@handlers[node.type]
+        handler.call(node, output)
+        break unless node.next?
+        node = node.next
+      end
+
+      output
+    end
+
+    def self.handle(node : Node, output : IO = IO::Memory.new)
+      if handler = @@handlers[node.type]?
+        handler.call(node, output)
+      else
+        puts "ignoring node type #{node.type}"
+      end
+      output
+    end
+
+    def self.handle(type : Type, &block : Handler)
+      @@handlers[type] = block
+    end
+
+    def self.children(node : Node)
+      if child = node.first_child?
+        loop do
+          yield child
+          break unless child.next?
+          child = child.next
+        end
+      end
+    end
+  end
+end
--- src/litmus/markdown_renderer.cr
+++ src/litmus/markdown_renderer.cr
@@ -1,111 +1,60 @@
-require "./renderer"
+require "./markd_renderer"
 
 module Litmus
-  class MarkdownRenderer < Renderer
-    class CodeBlock < Formatter
-      def format(text)
-        "#{"    " * @depth}#{text}"
+  class MarkdownRenderer < MarkdRenderer
+    handle Type::Document do |node, io|
+      contents = [] of IO
+
+      children(node) do |child|
+        contents << handle(child)
       end
+
+      # make sure there is spacing between content.
+      io << contents.join("\n\n")
     end
 
-    class BlockQuote < Formatter
-      def format(text)
-        "#{"> " * @depth}#{text}"
+    handle Type::Heading do |node, io|
+      contents = [] of IO
+
+      # build header
+      level = node.data["level"]
+      level = 1 unless level.is_a? Int
+      header = "#" * level + " "
+      contents << IO::Memory.new(header)
+
+      # gather header text
+      children(node) do |child|
+        contents << handle(child)
       end
+
+      io << contents.join
     end
 
-    class Heading < Formatter
-      def format(text)
-        "#{"#" * @depth} #{text}"
+    handle Type::Paragraph do |node, io|
+      contents = [] of IO
+
+      children(node) do |child|
+        contents << handle(child)
       end
 
-      def leave; 0; end
+      io << contents.join
     end
 
-    class BulletList < Formatter
-      def format(text)
-        "#{"    " * (@depth - 1)}- #{text}"
-      end
+    handle Type::Text do |node, io|
+      io << node.text
     end
 
-    node Type::CodeBlock do |action, render|
-      action.before do |node|
-        render.pad!
-        if node.fenced?
-          render.format! "```#{node.fence_language}"
-          render.newline!
-          render.format! node.text.chomp
-          render.newline!
-          render.format! "```"
-        else
-          render.format! node.text.chomp, fmt: CodeBlock.new
-        end
-        render.pad(2)
-      end
-    end
+    handle Type::BlockQuote do |node, io|
+      contents = [] of IO
 
-    node Type::BlockQuote do |action, render|
-      action.before do |node|
-        render.pad!
-        render.enter BlockQuote.new
+      children(node) do |child|
+        content = IO::Memory.new
+        content << "> "
+        content << handle(child).to_s.split("\n").join("\n> ")
+        contents << content
       end
 
-      action.after do |node|
-        render.leave BlockQuote
-        render.pad(2)
-      end
-    end
-
-    node Type::Heading do |action, render|
-      action.before do |node|
-        render.pad!
-        level = node.data["level"]?
-        level = 1 unless level.is_a? Int32
-        render.enter Heading.new(level)
-      end
-
-      action.after do |node|
-        render.leave Heading
-        render.pad(2)
-      end
-    end
-
-    node Type::Text do |action, render|
-      action.before do |node|
-        render.format! node.text
-      end
-    end
-
-    node Type::Paragraph do |action, render|
-      action.before do |node|
-        render.pad!
-      end
-
-      action.after do |node|
-        render.pad(2)
-      end
-    end
-
-    node Type::List do |action, render|
-      action.before do |node|
-        #render.pad!
-        render.enter BulletList.new
-      end
-
-      action.after do |node|
-        render.leave BulletList
-        render.pad(2)
-      end
-    end
-
-    node Type::Item do |action, render|
-      action.before do |node|
-        render.pad!
-      end
-
-      action.after do |node|
-        render.pad(1)
-      end
+      io << contents.join("\n> \n")
     end
   end
 end
